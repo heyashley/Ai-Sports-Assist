816812fa897875643cbb3c91a242fa6bc4c0f766
heyashley <142677709+heyashley@users.noreply.github.com>
2025-09-10 13:05:56 +0530

    fixes in entry screen

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
+      template: "./public/index.html",
       filename: "index.html",
     }),
   ],
